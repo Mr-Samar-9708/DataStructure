b59d78e83b8e7ebced6e8364dd2bf4286642438a
Samar Pratap Singh <152424550+Mr-Samar-9708@users.noreply.github.com>
2026-08-11 00:30:50 +0530

    Initial commit

---
+++ isDisarium.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    public boolean isDisarium(int n) {
+        
+        int sum  = 0;
+        
+        int temp = n;
+        while(temp != 0){
+            
+            int position = digitPosition(temp, temp%10);
+            int pow = 1;
+            int digit = temp%10;
+            
+            for(int i = 0; i < position; i++){
+                pow *= digit;
+            }
+            
+            sum += pow;
+            
+            temp = temp/10;
+        }
+        
+        return n == sum;
+    }
+    
+    public int digitPosition(int num, int n){
+        
+        String nums = Integer.toString(num);
+        
+        int position = nums.indexOf(String.valueOf(n));
+        
+        return position + 1;
+    }
+}
