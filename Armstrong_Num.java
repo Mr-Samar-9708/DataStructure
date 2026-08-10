f91491aab2a5803ab8b2231005b3293182b7e355
Samar Pratap Singh <152424550+Mr-Samar-9708@users.noreply.github.com>
2026-08-10 20:29:39 +0530

    Initial commit

---
+++ Armstrong_Num.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    static boolean armstrongNumber(int n) {
+        int size = 0;
+		    int sum = 0;
+		    int temp = n;
+
+		    while (temp > 0) {
+		    	size++;
+			    temp = temp / 10;
+		    }
+
+		    temp = n;
+
+		    while (temp > 0) {
+
+			    int digit = temp % 10;
+          int pow = 1;
+			    for (int i = 0; i < size; i++) {
+				      pow *= digit;
+			    }
+
+			    sum += pow;
+
+			    temp = temp / 10;
+		    }
+
+		    return n == sum;
+    }
+}
