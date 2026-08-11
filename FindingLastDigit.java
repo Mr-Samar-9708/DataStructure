27bb285360e216732713733dc94e960bf21d8215
Samar Pratap Singh <152424550+Mr-Samar-9708@users.noreply.github.com>
2026-08-11 14:09:07 +0530

    Initial commit

--- FindingLastDigit.java
+++ FindingLastDigit.java
@@ -1,9 +1,13 @@
+import java.util.*;
+
 class FindingLastDigit{
   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
 
-        System.out.println(n % 10);
+        System.out.println(Math.abs(n % 10));
     }
+  
 }
