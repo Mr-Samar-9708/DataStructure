4d20b1912895d0935be4cb7de68c74c40139b953
Samar Pratap Singh <152424550+Mr-Samar-9708@users.noreply.github.com>
2026-08-11 13:46:44 +0530

    Initial commit
    You are given a String S, you need to print its characters at even indices(index starts at 0).

---
+++ EvenPositionedChar.java
@@ -0,0 +1,13 @@
+public class EvenPositionedChar {
+  
+    public static void utility(String s) {
+        // code here
+        for(int i = 0; i< s.length(); i++){
+            
+            if(i % 2 == 0){
+                System.out.print(s.charAt(i));
+            }
+        }
+    }
+  
+}
