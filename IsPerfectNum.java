5aef2960c2023867a6980ba1d566d9e7bf368ee4
Samar Pratap Singh <152424550+Mr-Samar-9708@users.noreply.github.com>
2026-09-05 18:32:47 +0530

    Implement method to check if a number is perfect

---
+++ IsPerfectNum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPerfect(int n) {
+
+        int sum = 0;
+
+        for(int i = 1; i <= n-1; i++){
+
+            if(n % i == 0){
+                sum += i;
+            }
+        }
+
+        return sum == n;
+    }
+}
