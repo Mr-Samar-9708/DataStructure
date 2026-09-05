5d662d2daa95506ae9b73dd1c1444d2968f98a1e
Samar Pratap Singh <152424550+Mr-Samar-9708@users.noreply.github.com>
2026-09-06 01:02:47 +0530

    Optimize isPerfect method for perfect number check
    Refactor isPerfect method to improve efficiency by iterating only up to the square root of n and initializing sum to 1.

--- IsPerfectNum.java
+++ IsPerfectNum.java
@@ -1,12 +1,30 @@
 class Solution {
+    // public boolean isPerfect(int n) {
+
+    //     int sum = 0;
+
+    //     for(int i = 1; i <= n-1; i++){
+
+    //         if(n % i == 0){
+    //             sum += i;
+    //         }
+    //     }
+
+    //     return sum == n;
+    // }
+
     public boolean isPerfect(int n) {
 
-        int sum = 0;
+        int sum = 1;
 
-        for(int i = 1; i <= n-1; i++){
+        for(int i = 2; i <= Math.sqrt(n); i++){
 
             if(n % i == 0){
                 sum += i;
+
+                if(i != (n/2)){
+                    sum += (n/i);
+                }
             }
         }
 
